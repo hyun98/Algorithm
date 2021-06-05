cd73088fcf8fdab703ede618e46fbe1dfd36d661
Henu <hyun0404woo@gmail.com>
2021-06-06 05:38:15 +0900

    0606

---
+++ BOJ/다이나믹 프로그래밍/계단수_1562.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+#define MOD 1000000000
+using namespace std;
+
+int N, cmp, res;
+long long dp[101][10][1 << 10];
+
+void solve(){
+    
+    for(int i = 1; i <= 9; i++){
+        dp[1][i][1 << i] = 1;
+    }
+    
+    for(int n = 2; n <= N; n++){
+        for(int i = 0; i <= 9; i++){
+            for(int k = 0; k < (1 << 10); k++){
+                
+                // k의 i번째 bit를 on 시킨다
+                int offset = k | (1 << i);
+                
+                // n자리 + i로 끝나고 + 기존 k 상태(여태 사용한 숫자의 모음)에서 숫자 i를 추가하는 경우는
+                // n-1자리 + i-1 or i+1로 끝나고 + 기존 상태의 값(숫자 i를 추가하지 않음) 을 더해줘야 한다.
+                if(i == 0){
+                    dp[n][i][offset] = (dp[n][i][offset] + dp[n-1][1][k]) % MOD;
+                }
+                else if(i == 9){
+                    dp[n][i][offset] = (dp[n][i][offset] + dp[n-1][8][k]) % MOD;
+                }
+                else{
+                    dp[n][i][offset] = (dp[n][i][offset] + dp[n-1][i-1][k]) % MOD;
+                    dp[n][i][offset] = (dp[n][i][offset] + dp[n-1][i+1][k]) % MOD;
+                }
+            }
+            
+            
+        }
+    }
+    
+    int offset = 0;
+    long long ans = 0;
+    for(int i = 0; i <= 9; i++){
+        ans = (ans + dp[N][i][cmp]) % MOD;
+    }
+    cout << ans;
+}
+
+int main(){
+    cin >> N;
+    
+    for(int i = 0; i <= 9; i++){
+        cmp |= (1 << i);
+    }
+    
+    solve();
+    
+    return 0;
+}
+
+//int offset = 0;
+//long long sum = 0;
+//
+//if(i == 0){
+//    for(int k = 0; k <= 9; k++){
+//        if(dp[n-1][i+1][1 << k] > 0){
+//            offset |= (1 << k);
+//            sum = (sum + dp[n][i-1][1 << k]) % MOD;
+//        }
+//    }
+//    dp[n][i][offset] = sum;
+//}
+//else if(i == 9){
+//    for(int k = 0; k <= 9; k++){
+//        if(dp[n-1][i-1][1 << k] > 0){
+//            offset |= (1 << k);
+//            sum = (sum + dp[n][i-1][1 << k]) % MOD;
+//        }
+//    }
+//    dp[n][i-1][offset] = sum;
+//}
+//else{
+//    for(int k = 0; k <= 9; k++){
+//        if(dp[n-1][i-1][1 << k] > 0){
+//            offset |= (1 << k);
+//            sum = (sum + dp[n][i-1][1 << k]) % MOD;
+//        }
+//    }
+//    dp[n][i][offset] = sum;
+//    
+//    offset = 0; sum = 0;
+//    for(int k = 0; k <= 9; k++){
+//        if(dp[n-1][i+1][1 << k] > 0){
+//            offset |= (1 << k);
+//            sum = (sum + dp[n][i-1][1 << k]) % MOD;
+//        }
+//    }
+//    dp[n][i][offset] = sum;
+//}

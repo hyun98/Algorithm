c9f87ebc6d7c348ec9fab523f3f3083b3d16df25
Henu <hyun0404woo@gmail.com>
2021-10-16 08:26:16 +0900

    1016

--- tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-            dp[i] += (Combination(i-1, j) * dp[j] * dp[i - 1 -
--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <list>
+#include <queue>
 #include <algorithm>
 #include <cstring>
 #define fasti ios_base::sync_with_stdio(false); cin.tie(0);
@@ -14,11 +14,17 @@ typedef long long ll;
 using namespace std;
 
 
+
+void input(){
+    
+}
+
 void solve(){
     
 }
 
 int main(){
+    input();
     solve();
     
     return 0;

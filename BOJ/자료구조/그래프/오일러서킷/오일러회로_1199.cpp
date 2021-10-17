6ae25241383eafb2312b89fd34e18128b31299c2
Henu <hyun0404woo@gmail.com>
2021-10-17 13:29:58 +0900

    1017

--- BOJ/자료구조/그래프/오일러서킷/오일러회로_1199.cpp
+++ BOJ/자료구조/그래프/오일러서킷/오일러회로_1199.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define pii pair<int, int>
+
 using namespace std;
 
+int N;
 vector<int> Eucircuit;
-int N, adj[1001][1001], degree[1001];
-bool visited[1001];
+
+vector<int> adj[1001];
+vector<pii > edge;
+int edgenum[1000001];
+int edgecount;
 
 void findEuler(int now){
-    visited[now] = true;
-    for(int i = 1; i <= N; i++){
-        if(!adj[now][i]) continue;
-        while(adj[now][i] > 0){
-            adj[now][i]--;
-            adj[i][now]--;
-            findEuler(i);
+    while(adj[now].size()){
+        int e = adj[now].back();
+        int u = edge[e].first;
+        int v = edge[e].second;
+        if(u > v) swap(u, v);
+        if(edgenum[e]){
+            edgenum[e]--;
+            if(u == now) findEuler(v);
+            else findEuler(u);
         }
+        else adj[now].pop_back();
     }
     Eucircuit.push_back(now);
 }
 
 bool input(){
     cin >> N;
+    int a;
     for(int i = 1; i <= N; i++){
+        int degree = 0;
         for(int j = 1; j <= N; j++){
-            cin >> adj[i][j];
-            degree[i] += adj[i][j];
+            cin >> a;
+            degree += a;
+            if(i >= j || !a) continue;
+            edge.push_back({i, j});
+            edgenum[edgecount] += a;
+            
+            adj[i].push_back(edgecount);
+            adj[j].push_back(edgecount++);
         }
-        if(degree[i] % 2) return false;
+        if(degree % 2) return false;
     }
     return true;
 }
--- algorithm.cpp
+++ algorithm.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <algorithm>
+#include <cstring>
+#define fasti ios_base::sync_with_stdio(false); cin.tie(0);
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define INF 1e9+7
 #define pii pair<int, int>
 
+typedef long long ll;
+// typedef pair<int, int> pii;
+
 using namespace std;
 
-int N;
-priority_queue<pii > info;
-bool check[10001];
+
 
 void input(){
-    cin >> N;
-    int p, d;
-    for(int i = 0; i < N; i++){
-        cin >> p >> d;
-        info.push({p, d});
-    }
+    
 }
 
 void solve(){
-    int earn = 0;
     
-    while(!info.empty()){
-        int money = info.top().first;
-        int nowd = info.top().second;
-        
-        while(nowd){
-            if(!check[nowd]){
-                earn += money;
-                check[nowd] = true;
-                break;
-            }
-            nowd--;
-        }
-        info.pop();
-    }
-    
-    cout << earn;
 }
 
 int main(){
-    fastio
     input();
     solve();
     

d9a32adaefe9c81c2106465600ac8be6ccb3d75d
Henu <hyun0404woo@gmail.com>
2021-11-04 23:21:50 +0900

    1104

--- algorithm.cpp
+++ algorithm.cpp
@@ -2,51 +2,28 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <cstring>
+#define fasti ios_base::sync_with_stdio(false); cin.tie(0);
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define INF 1e9+7
+#define pii pair<int, int>
+
+typedef long long ll;
+// typedef pair<int, int> pii;
 
 using namespace std;
 
-int N, L, P;
-int info[1000001];
+
 
 void input(){
-    cin >> N;
-    int a, b;
-    for(int i = 0; i < N; i++){
-        cin >> a >> b;
-        info[a] = b;
-    }
-    cin >> L >> P;
     
 }
 
 void solve(){
-    priority_queue<int> pq;
-    int now_fuel = P;
-    int ans = 0;
     
-    for(int i = 1; i < L; i++){
-        now_fuel--;
-        if(info[i]) pq.push(info[i]);
-        
-        if(!now_fuel){
-            if(pq.empty()){
-                cout << -1;
-                return;
-            }
-            else{
-                ans++;
-                now_fuel += pq.top();
-                pq.pop();
-            }
-        }
-    }
-    
-    cout << ans;
 }
 
 int main(){
-    fastio
     input();
     solve();
     

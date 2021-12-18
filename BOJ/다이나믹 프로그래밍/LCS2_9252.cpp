5f83c5393ab97f121e10631be1d107cd84c14efa
Henu <hyun0404woo@gmail.com>
2021-12-19 01:12:59 +0900

    1219

--- BOJ/다이나믹 프로그래밍/LCS2_9252.cpp
+++ BOJ/다이나믹 프로그래밍/LCS2_9252.cpp
@@ -7,19 +7,13 @@ string first, second, LCS = "";
 int dp[1001][1001];
 
 void findLCS(){
-    // dp 배열을 모두 0으로 초기화
     memset(dp, 0, sizeof(dp));
     
-    // LCS표를 구하는 과정
     for(int row = 1; row <= first.size(); row++){
         for(int col = 1; col <= second.size(); col++){
             if(first[row-1] == second[col-1]){
-                // 만약 같은 문자가 나오면 두 문자열에서 
-                // 해당 두 문자를 비교하기 전의 LCS의 길이에 1을 더한다
                 dp[row][col] = dp[row-1][col-1] + 1;
             }
-            // 문자가 다르다면 first[row-1]까지의 문자열 + second[col] 까지의 문자열 간의 LCS값과
-            // first[row]까지의 문자열 + second[col+1] 까지의 문자열 간의 LCS값 중 큰값을 저장
             else dp[row][col] = max(dp[row-1][col], dp[row][col-1]);
         }
     }
@@ -29,7 +23,6 @@ void findLCS(){
     for(int row = first.size(); row >= 1; row--){
         for(int col = f_idx; col >= 1; col--){
             if(dp[row][col] == dp[row-1][col]){
-                // 바로 위 dp값이 같으면 현재 열 값을 유지한다
                 f_idx = col;
                 break;
             }

12b7ad7950bdb4feb94cc74eedab70f001b5add3
Henu <hyun0404woo@gmail.com>
2021-12-21 23:53:42 +0900

    1221

--- algorithm.cpp
+++ algorithm.cpp
@@ -13,17 +13,60 @@ typedef long long ll;
 
 using namespace std;
 
+int N, Q;
 
+vector<pii > adj[5001];
 
 void input(){
+    cin >> N >> Q;
+    int p, q, r;
+    for(int i = 0; i < N-1; i++){
+        cin >> p >> q >> r;
+        adj[p].push_back({r, q});
+        adj[q].push_back({r, p});
+    }
+}
+
+int bfs(int k, int v){
+    int cnt = 0;
     
+    bool visited[5001];
+    memset(visited, 0, sizeof(visited));
+    visited[v] = true;
+    
+    queue<int> que;
+    que.push(v);
+    
+    while(!que.empty()){
+        int now = que.front();
+        que.pop();
+        
+        for(int i = 0; i < adj[now].size(); i++){
+            int next = adj[now][i].second;
+            int next_road = adj[now][i].first;
+            
+            if(visited[next]) continue;
+            if(next_road < k) continue;
+            
+            cnt++;
+            visited[next] = true;
+            que.push(next);
+        }
+    }
+    
+    return cnt;
 }
 
 void solve(){
-    
+    int k, v;
+    for(int i = 0; i < Q; i++){
+        cin >> k >> v;
+        cout << bfs(k, v) << "\n";
+    }
 }
 
 int main(){
+    fastio
     input();
     solve();
     

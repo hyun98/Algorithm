87cc61e513635cdb99503a5a22a09350e85f42ab
hyun0404woo <hyun0404woo@gmail.com>
2022-05-09 20:40:53 +0900

    0509

--- algorithm.cpp
+++ algorithm.cpp
@@ -13,67 +13,74 @@ typedef long long ll;
 
 using namespace std;
 
-struct W{
-    int r1, c1, r2, c2;
+struct Edge{
+    int u, v;
+    ll cost;
 };
 
-int N, M;
-int Map[6][6];
-int answer;
+bool cmp(Edge &a, Edge &b){
+    return a.cost < b.cost;
+}
 
-W wings[4] = {
-    {0, -1, 1, 0},
-    {0, -1, -1, 0},
-    {-1, 0, 0, 1},
-    {1, 0, 0, 1}
-};
+int V, E;
+vector<Edge> lines;
+int node[10001];
 
 void input(){
-    cin >> N >> M;
-    int k;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            cin >> Map[i][j];
-        }
+    cin >> V >> E;
+    int a, b;
+    ll c;
+    for(int i = 0; i < E; i++){
+        cin >> a >> b >> c;
+        lines.push_back({a, b, c});
     }
+    sort(lines.begin(), lines.end(), cmp);
+    memset(node, -1, sizeof(node));
 }
 
-void dfs(int r, int c, int visited, int sum){
-    answer = max(answer, sum);
-    
-    if(r == N) return;
-    if(c == M){
-        dfs(r+1, 0, visited, sum);
-        return;
-    }
-    
-    // 현재 칸이 중심
-    for(int i = 0; i < 4; i++){
-        int nr1 = r + wings[i].r1;
-        int nc1 = c + wings[i].c1;
-        int nr2 = r + wings[i].r2;
-        int nc2 = c + wings[i].c2;
-        if(nr1 < 0 || nc1 < 0 || nr2 < 0 || nc2 < 0 || \
-           nr1 >= N || nc1 >= M || nr2 >= N || nc2 >= M) continue;
-        int tempv = ((1 << (r * M + c)) | (1 << (nr1 * M + nc1))) | (1 << (nr2 * M + nc2));
-        if(tempv & visited) continue;
-        
-        int tsum = Map[r][c] * 2 + Map[nr1][nc1] + Map[nr2][nc2];
-        dfs(r, c+1, visited | tempv, sum + tsum);
-    }
-    
-    // 현재 칸은 중심 x
-    dfs(r, c+1, visited, sum);
-    
+int topOftheNode(int a){
+    if(node[a] < 0) return a;
+    return node[a] = topOftheNode(node[a]);
 }
 
+bool unionNode(int a, int b){
+    a = topOftheNode(a);
+    b = topOftheNode(b);
+    
+    if(a == b) return false;
+    
+    if(node[a] > node[b]){
+        node[b] += node[a];
+        node[a] = b;
+    }
+    else{
+        node[a] += node[b];
+        node[b] = a;
+    }
+    
+    return true;
+}
 
 void solve(){
-    dfs(0, 0, 0, 0);
-    cout << answer << '\n';
+    int cnt = 0;
+    ll res = 0;
+    for(int i = 0; i < lines.size(); i++){
+        if(cnt == V-1) break;
+        
+        int lu = lines[i].u;
+        int lv = lines[i].v;
+        
+        if(unionNode(lu, lv)){
+            res += lines[i].cost;
+            cnt++;
+        }
+    }
+    
+    cout << res << '\n';
 }
 
 int main(){
+    fastio
     input();
     solve();
     
